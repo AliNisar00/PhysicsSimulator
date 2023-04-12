aeaed883697d10e461a72b142ddca23b3497b131
Ali Nisar <syed.alinisar2002@gmail.com>
2023-04-12 10:50:04 +0500

    test 1

--- testBox2DSDL/Source.cpp
+++ testBox2DSDL/Source.cpp
@@ -1,126 +1,180 @@
-#include <SDL.h>
-#include <box2d.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const int SCREEN_FPS = 60;
+#include <iostream>
+
+#include <box2d.h>
+//#include <DebugDraw.h>
+#include <SDL.h>
+
+using namespace std;
+
+#define PTM_RATIO 32.0
+
+SDL_Window* GameWin;
+SDL_Renderer* GameRen;
+SDL_Event event;
+
+int Width = 800, Height = 600;
+bool GameRunning = true;
+
+SDL_Rect box;
+SDL_Rect ground;
+SDL_Surface* Ball;
+SDL_Texture* Tex;
+SDL_Point center;
+
+int box_x, box_y, box_w, box_h;
+int ground_x, ground_y, ground_w, ground_h;
 
 int main(int argc, char* argv[])
 {
-    // Initialize SDL
-    SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		exit(0);
+	}
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("Box2D + SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	GameWin = SDL_CreateWindow("SDL Physics", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, SDL_WINDOW_SHOWN);
+	GameRen = SDL_CreateRenderer(GameWin, -1, SDL_RENDERER_PRESENTVSYNC);
 
-    // Create a renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	box_x = 0, box_y = 0;
+	box_w = 10, box_h = 10;
 
-    // Create a Box2D world
-    b2Vec2 gravity(0.0f, 9.81f);
-    b2World world(gravity);
+	box.x = box_x, box.y = box_y;
+	box.w = box_w, box.h = box_h;
 
-    // Create a ground body
-    b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, SCREEN_HEIGHT);
-    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+	ground_x = 0, ground_y = 540;
+	ground_w = 800, ground_h = 560;
 
-    // Create a ground shape
-    b2PolygonShape groundShape;
-    groundShape.SetAsBox(SCREEN_WIDTH, 10.0f);
+	ground.x = ground_x, ground.y = ground_y;
+	ground.w = ground_w, ground.h = ground_h;
 
-    // Add the ground fixture to the ground body
-    groundBody->CreateFixture(&groundShape, 0.0f);
+	Ball = SDL_LoadBMP("ball.bmp");
+	Tex = SDL_CreateTextureFromSurface(GameRen, Ball);
 
-    // Create a dynamic body
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-    b2Body* body = world.CreateBody(&bodyDef);
+	center.x = box.w * 0.5f;
+	center.y = box.h - (box.w * 0.5);
 
-    // Create a polygon shape for the body
-    b2PolygonShape polygonShape;
-    polygonShape.SetAsBox(50.0f, 50.0f);
+	b2Vec2 gravity(0, 9.8f);
+	bool doSleep = true;
 
-    // Add the polygon fixture to the body
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &polygonShape;
-    fixtureDef.density = 1.0f;
-    fixtureDef.friction = 0.3f;
-    body->CreateFixture(&fixtureDef);
+	b2World* World;
+	World = new b2World(gravity);
+	World->SetGravity(gravity);
 
-    // Set up the game loop
-    bool quit = false;
-    SDL_Event e;
-    Uint32 startTime, endTime, frameTime, sleepTime;
+	b2Body* groundBody;
+	b2BodyDef groundBodyDef;
+	groundBodyDef.type = b2_staticBody;
+	groundBodyDef.position.Set((float)ground_x, (float)ground_y);
+	groundBody = World->CreateBody(&groundBodyDef);
 
-    while (!quit)
-    {
-        // Get the start time of the frame
-        startTime = SDL_GetTicks();
+	b2Body* Body;
+	b2BodyDef ballBodyDef;
+	ballBodyDef.type = b2_dynamicBody;
+	b2Vec2 ballVector;
+	ballVector.Set(10, 10);
+	ballBodyDef.angularVelocity = 0.0f;
+	ballBodyDef.linearVelocity = ballVector;
 
-        // Process events
-        while (SDL_PollEvent(&e) != 0)
-        {
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-        }
+	ballBodyDef.position.Set(0, 0);
+	ballBodyDef.awake = true;
+	Body = World->CreateBody(&ballBodyDef);
 
-        // Clear the screen
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
-        SDL_RenderClear(renderer);
+	b2PolygonShape groundBox;
+	b2FixtureDef boxShapeDef;
+	boxShapeDef.shape = &groundBox;
+	boxShapeDef.density = 2.0f;
+	boxShapeDef.restitution = 0.5f;
+	groundBox.SetAsBox(800, 0); //This is what I needed to fix. Orignally it was (0,800)
 
-        // Step the Box2D world
-        world.Step(1.0f / SCREEN_FPS, 6, 2);
+	groundBody->CreateFixture(&groundBox, 0);
 
-        // Draw the ground
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        b2Vec2 groundVertices[4];
-        for (int i = 0; i < 4; i++)
-        {
-            groundVertices[i] = groundBody->GetWorldPoint(groundShape.m_vertices[i]);
-        }
-        SDL_RenderDrawLines(renderer, (SDL_Point*)groundVertices, 5);
+	b2PolygonShape dynamicBox;
 
-        // Draw the body
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-        b2Vec2 bodyVertices[4];
-        for (int i = 0; i < polygonShape.m_count; i++)
-        {
-            bodyVertices[i] = body->GetWorldPoint(polygonShape.m_vertices[i]);
-        }
-        SDL_RenderDrawLines(renderer, (SDL_Point*)bodyVertices, polygonShape.m_count + 1);
+	dynamicBox.SetAsBox(10.0f, 10.0f);
 
-        // Update the screen
-        SDL_RenderPresent(renderer);
+	b2FixtureDef fixtureDef;
 
-        // Get the end time of the frame
-        endTime = SDL_GetTicks();
+	fixtureDef.shape = &dynamicBox;
 
-        // Calculate the time taken to render the frame
-        frameTime = endTime - startTime;
+	fixtureDef.density = 2.0f;
+	fixtureDef.friction = 1.0f;
+	fixtureDef.restitution = 0.5f;
 
-        // Calculate the amount of time to sleep to maintain a constant frame rate
-        sleepTime = (1000 / SCREEN_FPS) - frameTime;
+	Body->CreateFixture(&fixtureDef); //this line
 
-        // If there is still time to sleep, then sleep
-        if (sleepTime > 0)
-        {
-            SDL_Delay(sleepTime);
-        }
-    }
+	float timeStep = 1.0f / 100.0f;
 
-    // Destroy the renderer
-    SDL_DestroyRenderer(renderer);
+	int velIter = 1.0;
+	int posIter = 1.0;
+	World->Step(timeStep, velIter, posIter);
 
-    // Destroy the window
-    SDL_DestroyWindow(window);
+	b2Vec2 pos = Body->GetPosition();
+	float an = Body->GetAngle();
 
-    // Quit SDL
-    SDL_Quit();
+	cout << "X:" << pos.x << endl;
+	cout << "Y:" << pos.y << endl;
 
-    return 0;
+	while (GameRunning)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				GameRunning = false;
+			}
 
+			if (event.type == SDL_KEYDOWN)
+			{
+				if (event.key.keysym.sym == SDLK_ESCAPE)
+				{
+					GameRunning = false;
+				}
+
+				if (event.key.keysym.sym == SDLK_LEFT)
+				{
+					Body->SetTransform(b2Vec2(-1, 0), 1);
+				}
+				else if (event.key.keysym.sym == SDLK_RIGHT)
+				{
+					Body->SetTransform(b2Vec2(+1, 0), 1);
+				}
+			}
+		}
+
+
+		World->Step(timeStep, velIter, posIter);
+
+		b2Vec2 pos = Body->GetPosition();
+		float angle = Body->GetAngle();
+
+		box.x = pos.x;
+		box.y = pos.y;
+
+		SDL_SetRenderDrawColor(GameRen, 0, 255, 255, 0);
+
+		SDL_RenderClear(GameRen);
+
+		//SDL_SetRenderDrawColor(GameRen, 0, 255, 0, 0);
+		//SDL_RenderFillRect(GameRen, &box);
+
+		SDL_RendererFlip Flip = SDL_FLIP_NONE;
+
+		SDL_RenderCopyEx(GameRen, Tex, NULL, &box, angle, &center, Flip);
+
+		SDL_SetRenderDrawColor(GameRen, 255, 0, 0, 0);
+		SDL_RenderFillRect(GameRen, &ground);
+
+		SDL_RenderPresent(GameRen);
+	}
+
+	SDL_DestroyTexture(Tex);
+	SDL_FreeSurface(Ball);
+
+	SDL_DestroyWindow(GameWin);
+	SDL_DestroyRenderer(GameRen);
+
+	SDL_Quit();
+
+	return 0;
 }
