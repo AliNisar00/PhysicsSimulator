bcd043dd3f36c3b926c41e1d46809f333ddeb23e
Ali Nisar <syed.alinisar2002@gmail.com>
2023-05-09 20:50:51 +0500

    try to introduce inheritence 1

--- testBox2DSDL/Source.cpp
+++ testBox2DSDL/Source.cpp
@@ -24,6 +24,57 @@ b2RevoluteJoint* joint2;
 b2RevoluteJoint* joint3;
 b2RevoluteJoint* joint4;
 
+class Shape
+{
+private:
+
+public:
+    virtual void draw() = 0;
+};
+
+class Square : public Shape
+{
+public:
+    b2Body* addRect(int x, int y, int w, int h, bool dyn = true)
+    {
+        b2BodyDef bodydef;
+        bodydef.position.Set(x * P2M, y * P2M);
+        if (dyn)
+            bodydef.type = b2_dynamicBody;
+        b2Body* body = world->CreateBody(&bodydef);
+
+        b2PolygonShape shape;
+        shape.SetAsBox(P2M * w / 2, P2M * h / 2);
+
+        b2FixtureDef fixturedef;
+        fixturedef.shape = &shape;
+        fixturedef.density = 1.0;
+        fixturedef.restitution = 0.0;
+        body->CreateFixture(&fixturedef);
+        return body;
+    }
+    void draw() {
+
+    }
+};
+
+Square square;
+
+class Triangle : public Shape
+{
+public:
+
+};
+
+class Circle : public Shape
+{
+public:
+
+};
+
+
+
+
 b2Body* addCircle(int x, int y, int r, bool dyn = true)
 {
     b2BodyDef bodydef;
@@ -99,7 +150,7 @@ void drawTriangle(b2Vec2 vertices[], b2Vec2 center)
 }
 
 
-
+/*
 b2Body* addRect(int x, int y, int w, int h, bool dyn = true)
 {
     b2BodyDef bodydef;
@@ -118,6 +169,7 @@ b2Body* addRect(int x, int y, int w, int h, bool dyn = true)
     body->CreateFixture(&fixturedef);
     return body;
 }
+*/
 
 
 void drawRect(b2Vec2 vertices[], b2Vec2 center)
@@ -135,7 +187,7 @@ void drawRect(b2Vec2 vertices[], b2Vec2 center)
 void addCar(int x, int y, int w, int h)
 {
     // Add the car body
-    carBody = addRect(x, y, w, h);
+    carBody = square.addRect(x, y, w, h);
 
     wheel1 = addCircle(x - w / 2, y + h / 2, h / 2.5);
     wheel2 = addCircle(x + w / 2, y + h / 2, h / 2.5);
