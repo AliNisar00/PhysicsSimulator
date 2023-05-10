90ee5859764120f105b41536478300db10f508cd
Ali Nisar <syed.alinisar2002@gmail.com>
2023-05-10 08:04:49 +0500

    Refactor: Shape parent class; Circle, Square, Triangle classes inherited from Shape; Borders inherited from Square; Car class multiple inherited from Square and Circle; relavent code modified and modularized.
    Issue: Display function cannot be broken down and refactored.
    
    Tasks remaining:
    1. Modularize all classes into their own header files and include them in the main Source file.
    2. Refactor Display().

--- testBox2DSDL/Source.cpp
+++ testBox2DSDL/Source.cpp
@@ -3,8 +3,8 @@
 #include <SDL_image.h>
 #include <box2d.h>
 
-const int WIDTH = 640;
-const int HEIGHT = 480;
+const int WIDTH = 1280;
+const int HEIGHT = 640;
 const float M2P = 20;
 const float P2M = 1 / M2P;
 
@@ -14,27 +14,21 @@ SDL_Renderer* renderer;
 
 b2Body* selectedBody = nullptr;
 
-b2Body* carBody;
-b2Body* wheel1;
-b2Body* wheel2;
-b2Body* wheel3;
-b2Body* wheel4;
-b2RevoluteJoint* joint1;
-b2RevoluteJoint* joint2;
-b2RevoluteJoint* joint3;
-b2RevoluteJoint* joint4;
 
 class Shape
 {
-private:
+protected:
 
 public:
-    virtual void draw() = 0;
+    virtual void displayShape() = 0;
 };
 
 class Square : public Shape
 {
+private:
+
 public:
+
     b2Body* addRect(int x, int y, int w, int h, bool dyn = true)
     {
         b2BodyDef bodydef;
@@ -53,16 +47,45 @@ public:
         body->CreateFixture(&fixturedef);
         return body;
     }
-    void draw() {
 
-    }
+    void displayShape()
+    {}
+
 };
 
-Square square;
 
 class Triangle : public Shape
 {
 public:
+
+    b2Body* addTriangle(int x, int y, int sideLength, bool dyn = true)
+    {
+        b2BodyDef bodydef;
+        bodydef.position.Set(x * P2M, y * P2M);
+        if (dyn)
+            bodydef.type = b2_dynamicBody;
+        b2Body* body = world->CreateBody(&bodydef);
+
+        b2Vec2 vertices[3];
+        float height = sideLength * sqrt(3.0f) / 2.0f;
+        vertices[0].Set(-sideLength / 2.0f, -height / 2.0f);
+        vertices[1].Set(0.0f, height / 2.0f);
+        vertices[2].Set(sideLength / 2.0f, -height / 2.0f);
+
+        b2PolygonShape shape;
+        shape.Set(vertices, 3);
+
+        b2FixtureDef fixturedef;
+        fixturedef.shape = &shape;
+        fixturedef.density = 1.0;
+        fixturedef.restitution = 1.0;
+        body->CreateFixture(&fixturedef);
+        return body;
+    }
+
+    void displayShape()
+    {}
+
 
 };
 
@@ -70,32 +93,92 @@ class Circle : public Shape
 {
 public:
 
+    b2Body* addCircle(int x, int y, int r, bool dyn = true)
+    {
+        b2BodyDef bodydef;
+        bodydef.position.Set(x * P2M, y * P2M);
+        if (dyn)
+            bodydef.type = b2_dynamicBody;
+        b2Body* body = world->CreateBody(&bodydef);
+
+        b2CircleShape shape;
+        shape.m_radius = r * P2M;
+        shape.m_p.Set(0, 0);
+
+        b2FixtureDef fixturedef;
+        fixturedef.shape = &shape;
+        fixturedef.density = 0.5;
+        fixturedef.friction = 0.8;
+        fixturedef.restitution = 1.0;
+        body->CreateFixture(&fixturedef);
+        return body;
+    }
+
+    void displayShape()
+    {}
+
+};
+
+class Car : public Square, public Circle {
+
+private:
+
+    Square* sq1;
+    Circle* c1;
+
+public:
+
+    b2Body* carBody;
+    b2Body* wheel1;
+    b2Body* wheel2;
+    b2Body* wheel3;
+    b2Body* wheel4;
+    b2RevoluteJoint* joint1;
+    b2RevoluteJoint* joint2;
+    b2RevoluteJoint* joint3;
+    b2RevoluteJoint* joint4;
+
+    void addCar(int x, int y, int w, int h)
+
+    {
+        // Add the car body
+        carBody = sq1->addRect(x, y, w, h);
+
+        wheel1 = c1->addCircle(x - w / 2, y + h / 2, h / 2.5);
+        wheel2 = c1->addCircle(x + w / 2, y + h / 2, h / 2.5);
+        //wheel3 = addCircle(x - w / 2, y - h / 2, h / 2.5); 
+        //wheel4 = addCircle(x + w / 2, y - h / 2, h / 2.5);
+
+        // Add the revolute joints
+        b2RevoluteJointDef jointdef1, jointdef2, jointdef3, jointdef4;
+        jointdef1.Initialize(carBody, wheel1, wheel1->GetWorldCenter());
+        jointdef2.Initialize(carBody, wheel2, wheel2->GetWorldCenter());
+        //jointdef3.Initialize(carBody, wheel3, wheel3->GetWorldCenter()); 
+        //jointdef4.Initialize(carBody, wheel4, wheel4->GetWorldCenter()); 
+        jointdef1.enableMotor = true;
+        jointdef2.enableMotor = true;
+        //jointdef3.enableMotor = true;
+        //jointdef4.enableMotor = true;
+        joint1 = (b2RevoluteJoint*)world->CreateJoint(&jointdef1);
+        joint2 = (b2RevoluteJoint*)world->CreateJoint(&jointdef2);
+        //joint3 = (b2RevoluteJoint*)world->CreateJoint(&jointdef3); 
+        //joint4 = (b2RevoluteJoint*)world->CreateJoint(&jointdef4); 
+    }
 };
 
 
-
-
-b2Body* addCircle(int x, int y, int r, bool dyn = true)
+void drawRect(b2Vec2 vertices[], b2Vec2 center)
 {
-    b2BodyDef bodydef;
-    bodydef.position.Set(x * P2M, y * P2M);
-    if (dyn)
-        bodydef.type = b2_dynamicBody;
-    b2Body* body = world->CreateBody(&bodydef);
-
-    b2CircleShape shape;
-    shape.m_radius = r * P2M;
-    shape.m_p.Set(0, 0);
-
-    b2FixtureDef fixturedef;
-    fixturedef.shape = &shape;
-    fixturedef.density = 0.5;
-    fixturedef.friction = 0.8;
-    fixturedef.restitution = 1.0;
-    body->CreateFixture(&fixturedef);
-    //body->GetUserData();
-    return body;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_Point points[5];
+    for (int i = 0; i < 4; i++)
+    {
+        points[i] = { (int)(vertices[i].x * M2P), (int)(vertices[i].y * M2P) };
+    }
+    points[4] = { (int)(vertices[0].x * M2P), (int)(vertices[0].y * M2P) };
+    SDL_RenderDrawLines(renderer, points, 5);
 }
+
 
 void drawCircle(SDL_Renderer* renderer, b2Vec2 center, float r, b2Vec2 offset = { 0,0 })
 {
@@ -112,30 +195,6 @@ void drawCircle(SDL_Renderer* renderer, b2Vec2 center, float r, b2Vec2 offset = 
     SDL_RenderDrawLines(renderer, vertices, PRECISION);
 }
 
-b2Body* addTriangle(int x, int y, int sideLength, bool dyn = true)
-{
-    b2BodyDef bodydef;
-    bodydef.position.Set(x * P2M, y * P2M);
-    if (dyn)
-        bodydef.type = b2_dynamicBody;
-    b2Body* body = world->CreateBody(&bodydef);
-
-    b2Vec2 vertices[3];
-    float height = sideLength * sqrt(3.0f) / 2.0f;
-    vertices[0].Set(-sideLength / 2.0f, -height / 2.0f);
-    vertices[1].Set(0.0f, height / 2.0f);
-    vertices[2].Set(sideLength / 2.0f, -height / 2.0f);
-
-    b2PolygonShape shape;
-    shape.Set(vertices, 3);
-
-    b2FixtureDef fixturedef;
-    fixturedef.shape = &shape;
-    fixturedef.density = 1.0;
-    fixturedef.restitution = 1.0;
-    body->CreateFixture(&fixturedef);
-    return body;
-}
 
 void drawTriangle(b2Vec2 vertices[], b2Vec2 center)
 {
@@ -150,75 +209,27 @@ void drawTriangle(b2Vec2 vertices[], b2Vec2 center)
 }
 
 
-/*
-b2Body* addRect(int x, int y, int w, int h, bool dyn = true)
+class Borders : public Square
 {
-    b2BodyDef bodydef;
-    bodydef.position.Set(x * P2M, y * P2M);
-    if (dyn)
-        bodydef.type = b2_dynamicBody;
-    b2Body* body = world->CreateBody(&bodydef);
 
-    b2PolygonShape shape;
-    shape.SetAsBox(P2M * w / 2, P2M * h / 2);
+private:
 
-    b2FixtureDef fixturedef;
-    fixturedef.shape = &shape;
-    fixturedef.density = 1.0;
-    fixturedef.restitution = 0.0;
-    body->CreateFixture(&fixturedef);
-    return body;
-}
-*/
+    Square* bords;
 
+public:
 
-void drawRect(b2Vec2 vertices[], b2Vec2 center)
-{
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_Point points[5];
-    for (int i = 0; i < 4; i++)
-    {
-        points[i] = { (int)(vertices[i].x * M2P), (int)(vertices[i].y * M2P) };
+    void addBorders() {
+        bords->addRect(WIDTH / 2, HEIGHT - 30, 1300, 10, false); //floor
+        bords->addRect(WIDTH / 30, 250, 10, 800, false); //leftBorder
+        bords->addRect(632, 250, 10, 800, false); //middle
+        bords->addRect(1236, 250, 10, 800, false); //rightBorder
+        bords->addRect(WIDTH / 2, 25, 1300, 10, false); //top
     }
-    points[4] = { (int)(vertices[0].x * M2P), (int)(vertices[0].y * M2P) };
-    SDL_RenderDrawLines(renderer, points, 5);
-}
 
-void addCar(int x, int y, int w, int h)
-{
-    // Add the car body
-    carBody = square.addRect(x, y, w, h);
-
-    wheel1 = addCircle(x - w / 2, y + h / 2, h / 2.5);
-    wheel2 = addCircle(x + w / 2, y + h / 2, h / 2.5);
-    //wheel3 = addCircle(x - w / 2, y - h / 2, h / 2.5); 
-    //wheel4 = addCircle(x + w / 2, y - h / 2, h / 2.5);
-
-    // Add the revolute joints
-    b2RevoluteJointDef jointdef1, jointdef2, jointdef3, jointdef4;
-    jointdef1.Initialize(carBody, wheel1, wheel1->GetWorldCenter());
-    jointdef2.Initialize(carBody, wheel2, wheel2->GetWorldCenter());
-    //jointdef3.Initialize(carBody, wheel3, wheel3->GetWorldCenter()); 
-    //jointdef4.Initialize(carBody, wheel4, wheel4->GetWorldCenter()); 
-    jointdef1.enableMotor = true;
-    jointdef2.enableMotor = true;
-    //jointdef3.enableMotor = true;
-    //jointdef4.enableMotor = true;
-    joint1 = (b2RevoluteJoint*)world->CreateJoint(&jointdef1);
-    joint2 = (b2RevoluteJoint*)world->CreateJoint(&jointdef2);
-    //joint3 = (b2RevoluteJoint*)world->CreateJoint(&jointdef3); 
-    //joint4 = (b2RevoluteJoint*)world->CreateJoint(&jointdef4); 
-}
-
-
-void addBorders() {
-
-    square.addRect(WIDTH / 2, HEIGHT - 30, 800, 10, false); //floor
-    square.addRect(WIDTH / 30, HEIGHT - 90, 10, 800, false); //leftBorder
-    square.addRect(618, HEIGHT - 90, 10, 800, false); //rightBorder
-    square.addRect(WIDTH / 2, 25, 800, 10, false); //top
-
-}
+    ~Borders() {
+        delete bords;
+    }
+};
 
 void selectBody(b2Vec2 point)
 {
@@ -239,12 +250,8 @@ void selectBody(b2Vec2 point)
 void init()
 {
     world = new b2World(b2Vec2(0.0f, 9.81f));
-
-    addBorders();
-
-    //   for (int i = 0; i < 250; i++) {
-    //       addCircle(WIDTH / 2, HEIGHT / 2, 8, true);
-    //   }
+    Borders b2;
+    b2.addBorders();
 }
 
 void destroyObjects()
@@ -282,7 +289,7 @@ void display()
                 }
                 drawRect(vertices, tmp->GetWorldCenter());
             }
-            else if (poly->m_count == 3) {
+            if (poly->m_count == 3) {
                 b2Vec2 vertices[3];
                 for (int i = 0; i < 3; i++)
                 {
@@ -317,12 +324,21 @@ int main(int argc, char** argv)
     bool gravityEnabled = true;
     bool leftMouseDown = false; // Flag to keep track of whether left mouse button is down
     bool rotateEnabled = false; // Flag to keep track of whether rotation is enabled or disabled
+    //Shape* sq = new Square;
+    Circle* cir = new Circle;
+    Square* sq = new Square;
+    Triangle* tri = new Triangle;
+    Borders* b1 = new Borders;
+    Car* car = new Car;
+
+
 
     while (running)
     {
         start = SDL_GetTicks();
         while (SDL_PollEvent(&event))
         {
+
             switch (event.type)
             {
             case SDL_QUIT:
@@ -331,13 +347,14 @@ int main(int argc, char** argv)
             case SDL_MOUSEBUTTONDOWN:
                 if (event.button.button == SDL_BUTTON_RIGHT) {
                     if (addingRect) {
-                        square.addRect(event.button.x, event.button.y, 15, 15, true);
+                        sq->addRect(event.button.x, event.button.y, 15, 15, true);
+
                     }
                     else if (addingTriangle) {
-                        addTriangle(event.button.x, event.button.y, 1, true);
+                        tri->addTriangle(event.button.x, event.button.y, 1, true);
                     }
                     else if (addingCircle) {
-                        addCircle(event.button.x, event.button.y, 8, true);
+                        cir->addCircle(event.button.x, event.button.y, 8, true);
                     }
                 }
                 if (event.button.button == SDL_BUTTON_LEFT) {
@@ -399,14 +416,14 @@ int main(int argc, char** argv)
                 }
                 else if (event.key.keysym.sym == SDLK_b)
                 {
-                    addCircle(WIDTH / 7, HEIGHT / 7, 2.5, true);
+                    cir->addCircle(WIDTH / 7, HEIGHT / 7, 2.5, true);
                 }
                 else if (event.key.keysym.sym == SDLK_r)
                 {
                     // destroy all Box2D objects
                     destroyObjects();
                     //add borders again
-                    addBorders();
+                    b1->addBorders();
                 }
                 else if (event.key.keysym.sym == SDLK_p) { // Toggle rotation
                     rotateEnabled = !rotateEnabled;
@@ -423,19 +440,23 @@ int main(int argc, char** argv)
 
                 }
                 else if (event.key.keysym.sym == SDLK_m) { // Toggle rotation
-                    addCar(WIDTH / 2, HEIGHT / 2, 90, 30);
+                    car->addCar(WIDTH - 350, HEIGHT / 2, 90, 30);
                 }
                 else if (event.key.keysym.sym == SDLK_LEFT) { // Move car backwards
-                    carBody->ApplyForceToCenter(b2Vec2(-45.0f, 0.0f), true);
+                    car->carBody->ApplyForceToCenter(b2Vec2(-150.0f, 0.0f), true);
                 }
                 else if (event.key.keysym.sym == SDLK_RIGHT) { // Move car forwards
-                    carBody->ApplyForceToCenter(b2Vec2(45.0f, 0.0f), true);
+                    car->carBody->ApplyForceToCenter(b2Vec2(150.0f, 0.0f), true);
                 }
                 else if (event.key.keysym.sym == SDLK_UP) { // Move car upwards in zero gravity
-                    carBody->ApplyForceToCenter(b2Vec2(.0f, -45.0f), true);
+                    car->carBody->ApplyForceToCenter(b2Vec2(.0f, -150.0f), true);
                 }
                 else if (event.key.keysym.sym == SDLK_DOWN) { // Move car downwards in zero gravity
-                    carBody->ApplyForceToCenter(b2Vec2(0.0f, 45.0f), true);
+                    car->carBody->ApplyForceToCenter(b2Vec2(0.0f, 150.0f), true);
+                }
+                else if (event.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    running = false;
                 }
                 break;
             }
@@ -466,6 +487,11 @@ int main(int argc, char** argv)
             SDL_Delay(1000.0f / 60.0f - (SDL_GetTicks() - start));
     }
 
+    delete sq;
+    delete b1;
+    delete cir;
+    delete tri;
+    delete car;
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
